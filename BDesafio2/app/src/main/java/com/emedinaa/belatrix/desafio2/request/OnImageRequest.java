6e52a49c4b0fa936068955ef4b7556859f940d3b
Eduardo Medina <em@osp.pe>
2015-08-28 10:20:59 -0500

    pruebas

---
+++ BDesafio2/app/src/main/java/com/emedinaa/belatrix/desafio2/request/OnImageRequest.java
@@ -0,0 +1,10 @@
+package com.emedinaa.belatrix.desafio2.request;
+
+/**
+ * Created by emedinaa on 28/08/15.
+ */
+public interface OnImageRequest {
+
+    void imageSuccess();
+    void imageError();
+}
